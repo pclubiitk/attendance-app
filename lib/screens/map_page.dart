ef95dbeaf867881f64f2adf4a8e9ff27146f3c8b
AkshatGupta15 <iitkakshat@gmail.com>
2025-01-23 22:34:03 +0530

    feat: use Event model details instead of hardcoding

--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -1,7 +1,5 @@
-
 import 'dart:async';
 import 'dart:developer';
-
 import 'package:attendance_app/location/event_service.dart';
 import 'package:attendance_app/model/event.dart';
 import 'package:flutter/material.dart';
@@ -23,12 +21,11 @@ class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
   Position? _currentPosition;
   LatLng? _locations;
-  final LatLng _eventLocation = LatLng(26.5086886, 80.2301569);
   bool _fetchingLocation = false;
   bool _markingAttendance = false;
   CameraPosition? _initialLocation;
   double distance = 0.0;
-  double eventRadius = 400.0;
+  double _eventRadius = 400.0;
   StreamSubscription<Position>? positionStream;
   String? _currentAddress;
   Event? event;
@@ -36,15 +33,19 @@ class _MapPageState extends State<MapPage> {
   @override
   void initState() {
     super.initState();
-    _getCurrentLocation();
-    _startLocationUpdates();
-    _fetchEvent();
+    _initialize();
   }
 
   @override
   void dispose() {
     positionStream?.cancel();
     super.dispose();
+  }
+
+  void _initialize() {
+    _getCurrentLocation();
+    _startLocationUpdates();
+    _fetchEvent();
   }
 
   void _onMapCreated(GoogleMapController controller) {
@@ -58,12 +59,14 @@ class _MapPageState extends State<MapPage> {
         target: _locations!,
         zoom: 15,
       );
-      distance = Geolocator.distanceBetween(
-        position.latitude,
-        position.longitude,
-        _eventLocation.latitude,
-        _eventLocation.longitude,
-      );
+      if (event != null) {
+        distance = Geolocator.distanceBetween(
+          position.latitude,
+          position.longitude,
+          event!.location.latitude,
+          event!.location.longitude,
+        );
+      }
       _fetchingLocation = false; // Fetching completed
       log("Location: ${position.latitude}, ${position.longitude}");
       log("Distance: $distance");
@@ -137,6 +140,11 @@ class _MapPageState extends State<MapPage> {
       Event fetchedEvent = await EventService().fetchEvent('1');
       setState(() {
         event = fetchedEvent;
+        _initialLocation = CameraPosition(
+          target: event!.location,
+          zoom: 15,
+        );
+        _eventRadius = event!.eventRadius;
       });
       log("Event fetched: ${event?.name}");
     } catch (e) {
@@ -183,7 +191,6 @@ class _MapPageState extends State<MapPage> {
                     Container(
                       width: screenWidth,
                       child: FCard(
-                        // style: FCardStyle.inherit(colorScheme: , typography: typography, style: style),
                         title: Text(event?.name ?? 'Event Details:'),
                         subtitle: Text(
                           event?.description != null &&
@@ -201,7 +208,7 @@ class _MapPageState extends State<MapPage> {
                                   const TextSpan(
                                     text: 'Start: ',
                                     style: TextStyle(
-                                      fontWeight: FontWeight.w500, 
+                                      fontWeight: FontWeight.w500,
                                       color: Color.fromARGB(255, 0, 0, 0),
                                       fontSize: 15,
                                     ),
@@ -224,7 +231,7 @@ class _MapPageState extends State<MapPage> {
                                   const TextSpan(
                                     text: 'Deadline: ',
                                     style: TextStyle(
-                                      fontWeight: FontWeight.w500, 
+                                      fontWeight: FontWeight.w500,
                                       color: Color.fromARGB(255, 0, 0, 0),
                                       fontSize: 15,
                                     ),
@@ -247,7 +254,7 @@ class _MapPageState extends State<MapPage> {
                                   const TextSpan(
                                     text: 'Location: ',
                                     style: TextStyle(
-                                      fontWeight: FontWeight.w500, 
+                                      fontWeight: FontWeight.w500,
                                       color: Color.fromARGB(255, 0, 0, 0),
                                       fontSize: 15,
                                     ),
@@ -284,14 +291,14 @@ class _MapPageState extends State<MapPage> {
                         markers: {
                           Marker(
                             markerId: MarkerId('Event Location'),
-                            position: _eventLocation,
+                            position: event?.location ?? LatLng(0, 0),
                           ),
                         },
                         circles: {
                           Circle(
                             circleId: CircleId("1"),
-                            center: _eventLocation,
-                            radius: eventRadius,
+                            center: event?.location ?? LatLng(0, 0),
+                            radius: event?.eventRadius ?? 400.0,
                             strokeWidth: 2,
                             strokeColor: Colors.blueAccent,
                             fillColor: const Color.fromARGB(96, 82, 240, 240),
@@ -306,11 +313,11 @@ class _MapPageState extends State<MapPage> {
                         fontWeight: FontWeight.w600,
                         fontSize: 15,
                         color:
-                            distance < eventRadius ? Colors.green : Colors.red,
+                            distance < _eventRadius ? Colors.green : Colors.red,
                       ),
                     ),
                     Text(
-                      "Your Location: $_currentAddress",
+                      "Your Location: ${_currentAddress ?? 'Loading...'}",
                       style: const TextStyle(
                         color: Colors.grey,
                         fontSize: 13,
@@ -321,10 +328,10 @@ class _MapPageState extends State<MapPage> {
                       prefix: _markingAttendance
                           ? const CircularProgressIndicator()
                           : null,
-                      label: distance < eventRadius
+                      label: distance < _eventRadius
                           ? const Text('Mark Attendance')
                           : const Text('Not in Event Location'),
-                      onPress: distance < eventRadius ? _markAttendance : null,
+                      onPress: distance < _eventRadius ? _markAttendance : null,
                     ),
                   ],
                 ),
